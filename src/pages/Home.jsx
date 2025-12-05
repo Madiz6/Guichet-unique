eb421cda21dacccd910d543c92c438e64a2d5457
Remoz Giovanni <remoz.giovanni@meras.io>
2025-12-05 16:29:32 +0000

    File changes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -302,7 +302,7 @@ export default function Home() {
             </motion.div>
           </div>
           
-          <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-8">
+          <div className="grid md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6">
             {[
               {
                 icon: Users,
