5c6b4cfe1b9665d7bbb3f421bc6f13038c3dd655
Remoz Giovanni <remoz.giovanni@meras.io>
2026-02-25 09:38:50 +0000

    File changes

--- src/pages/Transactions.jsx
+++ src/pages/Transactions.jsx
@@ -471,7 +471,7 @@ export default function Transactions() {
       <TransactionDetailDrawer
         transaction={selectedTransaction}
         onClose={() => setSelectedTransaction(null)}
-        onUpdate={(data) => updateMutation.mutate({ id: data.id || selectedTransaction.id, data })}
+        onSave={(id, data) => updateMutation.mutate({ id, data })}
         onDelete={(id) => {
           if (confirm('Supprimer cette transaction?')) {
             deleteMutation.mutate(id);
